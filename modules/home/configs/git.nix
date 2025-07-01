4d8f4725cbd9006e1acf7f3880604e4534a0e341
Timbits <timbits1123@gmail.com>
2025-06-30 23:26:58 -0400

    home-manager 2025-06-30 23:26

--- modules/home/configs/git.nix
+++ modules/home/configs/git.nix
@@ -30,6 +30,12 @@ in
         signByDefault = true;
       };
 
+      aliases = {
+        lg = "log --graph --relative-date --pretty=format:'%C(auto)%h %d %C(reset blue)(%ar) %C(bold yellow)%an%C(reset) %s'";
+        lgd = "log --graph --pretty=format:'%C(yellow)commit %H %C(auto)%d%n%C(bold magenta)Author:    %an <%ae> %C(reset)on %C(blue)%aD%n%C(bold green)Committer: %cn <%ce> %C(reset)on %C(blue)%aD%n%n    %B%n'";
+        lgds = "log --graph --pretty=format:'%C(yellow)commit %H %C(auto)%d%n%C(bold magenta)Author:    %an <%ae> %C(reset)on %C(blue)%aD%n%C(bold green)Committer: %cn <%ce> %C(reset)on %C(blue)%aD%n%C(red)%GG%n    %B%n'";
+      };
+
       extraConfig = {
         gpg = {
           format = "ssh";
