7300274e8134d55474f94149ac026bada3533cf7
Timbits <timbits1123@gmail.com>
2025-08-29 21:52:49 -0400

    home-manager 2025-08-29 21:52

--- derivations/nixCats/lua/config/scripts/init.lua
+++ derivations/nixCats/lua/config/scripts/init.lua
@@ -1,5 +1,6 @@
 require('config.scripts.eol_marker')
-require('config.scripts.diagnostic_sign_column')
+-- disabled temporarily
+-- require('config.scripts.diagnostic_sign_column')
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
